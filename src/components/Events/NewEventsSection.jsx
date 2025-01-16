9354911e6251163ca4f69ffdf44dbbda89c0872a
ddangmul <j.ddangmul@gmail.com>
2025-01-16 14:20:59 +0900

    리액트 쿼리 캐시처리 기능 사용: staleTime, gcTime

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -16,6 +16,8 @@ export default function NewEventsSection() {
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["events"],
     queryFn: fetchEvents,
+    staleTime: 5000,
+    // gcTime: 1000,
   });
 
   let content;
