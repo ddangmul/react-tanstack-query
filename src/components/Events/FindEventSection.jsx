a5d2c59a75b793ddaf32914939e72c14ef1c93dc
ddangmul <j.ddangmul@gmail.com>
2025-01-16 15:39:06 +0900

    FindEventSection에서 동적 쿼리함수 및 쿼리키 설정 & 쿼리 구성 객체 및 요청 취소 l 전달

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -1,10 +1,52 @@
-import { useRef } from 'react';
+import { useRef, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchEvents } from "../../util/http";
+import LoadingIndicator from "../UI/LoadingIndicator";
+import ErrorBlock from "../UI/ErrorBlock";
+import EventItem from "./EventItem";
 
 export default function FindEventSection() {
   const searchElement = useRef();
+  const [searchTerm, setSearchTerm] = useState(""); // queryKey를 동적으로 설정할 때 Ref는 해당 컴포넌트를 재실행시키지 못하므로 참조값을 상태값에 저장
+
+  const { data, isPending, isError, error } = useQuery({
+    // 쿼리키
+    // events: NewEventsSection 쿼리 데이터를 재사용하되,
+    // { search: setSearchTerm } : 사용자가 새로운 검색어를 입력하면 쿼리함수가 재실행 + NewEventsSection에서 이 데이터를 재사용하지 못하게 함
+    queryKey: ["events", { search: searchTerm }],
+    queryFn: ({ signal }) => fetchEvents({ signal, searchTerm }),
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
+    setSearchTerm(searchElement.current.value);
+  }
+
+  let content = <p>Please enter a search term and to find events.</p>;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message || "Failed to fetch events."}
+      />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
   }
 
   return (
@@ -20,7 +62,7 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
