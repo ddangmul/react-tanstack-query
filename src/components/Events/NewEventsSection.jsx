f0cb9c13fb270fd3eb26c3c8e1de2f772805d8e8
ddangmul <j.ddangmul@gmail.com>
2025-01-16 13:50:09 +0900

    리액트 쿼리 사용해 NewEventsSection 데이터 가져오기

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -1,52 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from "@tanstack/react-query";
 
-import LoadingIndicator from '../UI/LoadingIndicator.jsx';
-import ErrorBlock from '../UI/ErrorBlock.jsx';
-import EventItem from './EventItem.jsx';
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import EventItem from "./EventItem.jsx";
+import { fetchEvents } from "../../util/http.js";
 
 export default function NewEventsSection() {
-  const [data, setData] = useState();
-  const [error, setError] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:3000/events');
-
-      if (!response.ok) {
-        const error = new Error('An error occurred while fetching the events');
-        error.code = response.status;
-        error.info = await response.json();
-        throw error;
-      }
-
-      const { events } = await response.json();
-
-      return events;
-    }
-
-    fetchEvents()
-      .then((events) => {
-        setData(events);
-      })
-      .catch((error) => {
-        setError(error);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []);
+  // useQuery : 훅 자체적으로 작동해서 HTTP요청을 전송 & 필요한 이벤트 데이터 로딩 & 로딩 상태에 대한 정보 제공(-> 요청 전송 중 발생한 오류 파악 가능)
+  // http 요청을 전송하는 로직은 내장되어 있지 않고, 요청을 관리하는 로직을 제공함
+  // 요청 전송 코드는 직접 작성 필요
+  // 객체를 전달
+  // isPending : 응답을 받았는지
+  // isError: 오류 응답을 받았는지
+  // error:  발생한 오류에 대한 정보 (ex-오류 메세지)
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events"],
+    queryFn: fetchEvents,
+  });
 
   let content;
 
-  if (isLoading) {
+  if (isPending) {
     content = <LoadingIndicator />;
   }
 
-  if (error) {
+  if (isError) {
     content = (
-      <ErrorBlock title="An error occurred" message="Failed to fetch events" />
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message || "Failed to fetch data."}
+      />
     );
   }
 
