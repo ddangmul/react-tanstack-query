4f44f03bb9ac5f07eb0bdd0583fb1088f7cb460c
ddangmul <j.ddangmul@gmail.com>
2025-01-20 12:19:28 +0900

    useMutation으로 이벤트 정보 업데이트 & onMutate. onError. onSettled로 낙관적 업데이트 작성

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,9 +1,9 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { fetchEvent } from "../../util/http.js";
+import { fetchEvent, updateEvent, queryClient } from "../../util/http.js";
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
@@ -16,7 +16,36 @@ export default function EditEvent() {
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
   });
 
-  function handleSubmit(formData) {}
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    // [onMutate] mutate가 호출될 때마다 실행
+    onMutate: async (data) => {
+      // data = 백엔드에 보낸 데이터 { id: params.id, event: formData }
+      const newEvent = data.event;
+
+      await queryClient.cancelQueries({ queryKey: ["events", params.id] });
+      // 롤백을 위해 기존 데이터 저장
+      const previousEvent = queryClient.getQueryData(["events", params.id]);
+      queryClient.setQueryData(["events", params.id], newEvent);
+
+      return { previousEvent }; // context로 전달
+    },
+    // [백엔드 실패 시 실행]
+    onError: (error, data, context) => {
+      queryClient.setQueryData(["events", params.id], context.previousEvent);
+    },
+    // [onSeltteld] 성공 여부와 상관 없이 mutation이 완료될 때마다 호출
+    // 백엔드에 있는 것과 동일한 데이터가 클라이언트에 있는지 확인 작업
+    // 롤백 후 백엔드에서 다른 작업을 실행해 데이터가 클라이언트와 동기화되지 않은 경우, 리액트 쿼리에 데이터를 내부적으로 다시 가져오도록 강제하여 다시 동기화
+    onSettled: () => {
+      queryClient.invalidateQueries(["events", params.id]);
+    },
+  });
+
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData });
+    navigate("../");
+  }
 
   function handleClose() {
     navigate("../");
